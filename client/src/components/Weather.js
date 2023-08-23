cfbbda32ef6ac495175e862c7644c77d8490a052
evmaperry <evmaperry@gmail.com>
2023-08-22 21:09:19 -0400

    added backend request to get weather data

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import axios from 'axios'
 
-export default function Weather () {
+export default function Weather() {
+
+  const [zip, setZip] = React.useState(49685)
+  
+  React.useEffect(() => {
+
+    const options = {
+      method: 'GET',
+      url: `http://localhost:5051/weather/${zip}`
+    }
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data)
+  }).catch(function (error) {
+        console.error(error);
+    }
+  )
+  }
+  )
 
   return (
     <div>
-        <h1>Sanity Check</h1>
+      <h1>Sanity Checks</h1>
     </div>
   )
 
--- server/routes/weather-data.mjs
+++ server/routes/weather-data.mjs
@@ -1,9 +1,35 @@
 import express from 'express'
+import fetch from 'node-fetch'
+import axios from 'axios'
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  res.send(results).status(200);
-})
+router.get('/49685', (req, res) => {
+  // res.status(404).send('Sorry, we cannot find that!')
+  const options = {
+    url: `https://weatherapi-com.p.rapidapi.com/current.json?q=49685`,
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': process.env.WEATHER_API_KEY,
+      'X-RapidAPI-Host': process.env.WEATHER_API_HOST
+    } 
+  }
+  
+  
+
+  axios.request(options).then(function (response) {
+    console.log(response)
+    res.json(response.data);
+}).catch(function (error) {
+    console.error(error);
+});
+  // const weather = await response.json()
+  // await console.log(weather);
+  // res.weather = weather;
+  // console.log('res', res);
+  // res.send(res);
+}
+
+)
 
 export default router
